dd1813b2d6093b899eb6669e184c2dbfcb731f03
Rafaelfurtadovs <rafael.santos@sou.inteli.edu.br>
2025-03-21 10:41:43 -0300

    feat: adiciona a funcao que realiza o teste

--- test.py
+++ test.py
@@ -7,3 +7,19 @@ load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL")
 TOKEN = os.getenv("TOKEN")
+
+def test_get_courses():
+    
+    url = f"{BASE_URL}/courses"
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+
+    # Fazendo a requisição GET
+    response = requests.get(url, headers=headers)
+
+    # Validações
+    assert response.status_code == 200, "Status code não é 200"
+    data = response.json()
+    assert isinstance(data, list), "Resposta não é uma lista"
+    assert len(data) > 0, "Nenhum curso encontrado"
+    
+    
